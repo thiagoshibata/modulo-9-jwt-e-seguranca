954e59fdcb8729e6a8557ee9402e3e41c60fe32f
thiagoshibata <thiago.shibatta@gmail.com>
2024-12-24 09:05:56 -0300

    feat: implementing route with controllers logic

--- src/controllers/balance_editor_controller.py
+++ src/controllers/balance_editor_controller.py
@@ -1,17 +1,20 @@
 from src.models.interfaces.user_repository import UserRepositoryInterface
-from .interfaces.balance_editor_controller import BalanceEditorInterface
+from .interfaces.balance_editor_controller import BalanceEditorControllerInterface
 
-class BalanceEditorController(BalanceEditorInterface):
+class BalanceEditorController(BalanceEditorControllerInterface):
     def __init__(self, user_repository: UserRepositoryInterface) -> None:
         self.__user_repository = user_repository
 
     def edit(self, user_id: int, new_balance: float) -> dict:
         self.__user_repository.edit_balance(user_id, new_balance)
-        self.__format_response(new_balance)
+        formated_response = self.__format_response(new_balance)
 
-    def __format_response(new_balance: float) -> dict:
+        return formated_response
+        
+    def __format_response(self, new_balance: float) -> dict:
         return {
             "type": "User",
             "count": 1,
             "new balance": new_balance
         }
+        
