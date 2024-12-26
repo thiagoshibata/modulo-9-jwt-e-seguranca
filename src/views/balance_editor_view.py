20395ccc544af447c20570058378d7e67bf103a9
thiagoshibata <thiago.shibatta@gmail.com>
2024-12-26 18:19:03 -0300

    feat: verify user_id and header_user_id

--- src/views/balance_editor_view.py
+++ src/views/balance_editor_view.py
@@ -10,14 +10,16 @@ class BalanceEditorView(ViewInterface):
     def handle(self, http_request: HttpRequest) -> HttpResponse:
         new_balance = http_request.body.get("new_balance")
         user_id = http_request.params.get("user_id")
+        header_user_id = http_request.headers.get("uid")
         self.__validate_inputs(new_balance, user_id)
 
         response = self.__controller.edit(user_id, new_balance)
         return HttpResponse(body={"data": response}, status_code=200)
 
-    def __validate_inputs(self, new_balance: any, user_id: any) -> None:
+    def __validate_inputs(self, new_balance: any, user_id: any, header_user_id: any ) -> None:
         if (
                 not new_balance
                 or not user_id
                 or not isinstance(new_balance, float)
+                or int(header_user_id) != int(user_id)
             ) :raise Exception("Invalid Input")   
